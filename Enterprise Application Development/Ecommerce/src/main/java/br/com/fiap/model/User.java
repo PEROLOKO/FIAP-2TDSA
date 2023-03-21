55809fb6ae3347007c322035e3224d4e30de0db7
Henry Kinoshita <42702870+PEROLOKO@users.noreply.github.com>
2023-03-21 11:52:40 -0300

    aula2103

---
+++ Enterprise Application Development/Ecommerce/src/main/java/br/com/fiap/model/User.java
@@ -0,0 +1,24 @@
+package br.com.fiap.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "User")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    private String name;
+    private String address;
+    private String phone;
+    @Column(unique = true)
+    private String email;
+}
